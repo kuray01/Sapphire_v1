dc5d57dbcce9f5681bdf231a05084d8e0fd9582f
AnitaPC <AnitaPC@DESKTOP-TQT5VK2>
2024-11-29 17:34:54 +0300

    Добавьте файлы проекта.

---
+++ Pages/register_1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sapphire_v1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для register_1.xaml
+    /// </summary>
+    public partial class register_1 : Window
+    {
+        public register_1()
+        {
+            InitializeComponent();
+        }
+
+        private void Register1Button_Click(object sender, RoutedEventArgs e)
+        {
+            register_2 registerWindow = new register_2();
+            registerWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Pages/register_2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sapphire_v1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для register_2.xaml
+    /// </summary>
+    public partial class register_2 : Window
+    {
+        public register_2()
+        {
+            InitializeComponent();
+        }
+
+        private void Register2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void user_name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
